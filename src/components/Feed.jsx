52ecd33016f5b5bbeb723ed1f74462eea095973c
1SiddhantSingh <siddhantsingh193726@gmail.com>
2023-04-18 18:41:04 +0530

    adding files

---
+++ src/components/Feed.jsx
@@ -0,0 +1,33 @@
+import { React, useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+const Feed = () => {
+  return (
+    <Stack
+      sx={{
+        flexDirection: {
+          sx: "column",
+          md: "row",
+        },
+      }}
+    >
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        Sidebar
+        <Typography
+          className="copyright"
+          variant="boby2"
+          sx={{ mt: 1.5, color: "#fff" }}>
+          Copyright @2023 Siddhant Singh
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
